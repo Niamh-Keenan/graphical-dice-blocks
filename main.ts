7f3dbf19460fff5e79f262d75e9e4c261c48b8b1
Niamh-Keenan <113034875+Niamh-Keenan@users.noreply.github.com>
2022-09-26 09:45:04 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+let Number2 = 0
+input.onGesture(Gesture.Shake, function () {
+    Number2 = randint(1, 6)
+    if (Number2 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Number2 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (Number2 == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Number2 == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Number2 == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (Number2 == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
